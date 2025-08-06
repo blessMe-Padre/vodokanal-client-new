51d8ab28778581066453c49d1947c03d801ddd5a
Сергей Субботин <zarodiny@yandex.ru>
2025-08-06 15:00:22 +1000

    feat: добавляет стили для страницы одной новости и улучшает структуру компонента новостей

--- src/app/(pages)/news/[id]/page.tsx
+++ src/app/(pages)/news/[id]/page.tsx
@@ -3,6 +3,9 @@ import Image from "next/image";
 import Breadcrumbs from "@/app/components/Breadcrumbs/Breadcrumbs";
 import ContentRenderer from "@/app/components/ContentRenderer/ContentRenderer";
 import fetchData from "@/app/utils/fetchData";
+import formatDate from '@/app/utils/formatDate'
+
+import styles from "../style.module.scss"
 
 export const revalidate = 600; // 10 минут
 
@@ -35,18 +38,25 @@ export default async function Page({ params }: { params: Promise<{ id: string }>
     return (
         <div className="container">
             <Breadcrumbs secondLink="/news" secondLabel="Новости" thirdLabel={page?.data?.title} />
-            <article>
-                <Image
-                    src={page?.data?.image?.url ? `${process.env.NEXT_PUBLIC_API_SERVER}${page.data.image.url}` : '/placeholder.svg'}
-                    alt={page?.data?.title}
-                    width={600}
-                    height={300}
-                    loading="lazy"
-                    placeholder="blur"
-                    blurDataURL="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTQ0MiIgaGVpZ2h0PSIxMTg5IiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxyZWN0IHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIGZpbGw9IiNjY2MiIC8+PC9zdmc+"
-                />
-                <h1>{page?.data?.title}</h1>
-                <ContentRenderer content={page?.data?.content} />
+            <article className={styles.article}>
+                <div className={`image-wrapper ${styles.article_img}`}>
+                    <Image
+                        src={page?.data?.image?.url ? `${process.env.NEXT_PUBLIC_API_SERVER}${page.data.image.url}` : '/placeholder.svg'}
+                        alt={page?.data?.title}
+                        width={600}
+                        height={300}
+                        loading="lazy"
+                        placeholder="blur"
+                        blurDataURL="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTQ0MiIgaGVpZ2h0PSIxMTg5IiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxyZWN0IHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIGZpbGw9IiNjY2MiIC8+PC9zdmc+"
+                        />
+                </div>
+                <div className={styles.article_content}>
+                    <div>
+                        <h1 className={styles.article_title}>{page?.data?.title}</h1>
+                        <ContentRenderer content={page?.data?.content} />
+                    </div>
+                    <p className={styles.item_date}>{formatDate(page?.data?.publishedAt)}</p>
+                </div>
             </article>
         </div>
     )
