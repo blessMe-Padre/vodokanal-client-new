0763969334175c538164b5794c03af5d29e3c35b
Сергей Субботин <zarodiny@yandex.ru>
2025-08-06 10:30:02 +1000

    feat: добавляет hero в about

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -4,6 +4,8 @@ import Breadcrumbs from "@/app/components/Breadcrumbs/Breadcrumbs";
 import ContentRenderer from "@/app/components/ContentRenderer/ContentRenderer";
 import fetchData from "@/app/utils/fetchData";
 
+import styles from './style.module.scss';
+
 export const metadata = {
     title: 'МУП "Находка-Водоканал" - О компании',
     description: 'О компании МУП "Находка-Водоканал"',
@@ -16,7 +18,16 @@ export default async function About() {
 
         <div className="container">
             <Breadcrumbs thirdLabel="О компании" />
-            <h1>О компании</h1>
+            <h1 className="visually-hidden">О компании</h1>
+
+            <section className={styles.about_section}>
+                <div className={styles.about_section_content}>
+                    <h2 className={styles.about_section_title}>Мы обеспечиваем город водой — каждый день, каждый дом</h2>
+                    <p><strong>МУП «Находка-Водоканал» — </strong> это стратегически важное предприятие, обеспечивающее бесперебойное водоснабжение и водоотведение для жителей, бизнеса 
+                    и социальных объектов города Находка.</p>
+                    <p><strong>Наша миссия — </strong> поставлять чистую воду, сохранять природные ресурсы и делать жизнь в городе комфортнее.</p>
+                </div>
+            </section>
 
             <Image
                 src={page?.data?.image?.url ? `${process.env.NEXT_PUBLIC_API_SERVER}${page.data.image.url}` : '/placeholder.svg'}
