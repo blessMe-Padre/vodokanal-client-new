9d344f6a2c261a831dbdbff57f3153b22fc59d0e
kkirrin <khlebnikov-2016@mail.ru>
2025-08-07 15:02:58 +1000

    feat: добавляет новые компоненты Kontrakt и News, обновляет импорт в Home, удаляет неиспользуемые файлы Contract и fetchData

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 
-import { MainBanner, Services } from "./section/index";
+import { Kontrakt, MainBanner, News, Services } from "./section/index";
 
 export default function Home() {
   return (
     <div className="container">
       <MainBanner />
       <Services />
+      <Kontrakt />
+      <News />
     </div>
   );
 }
--- src/app/section/index.tsx
+++ src/app/section/index.tsx
@@ -1,3 +1,4 @@
 export { default as News } from './News/News';
 export { default as MainBanner } from './MainBanner/MainBanner'
 export { default as Services } from './Services/Services';
+export { default as Kontrakt } from './Kontrakt/Kontrakt';
