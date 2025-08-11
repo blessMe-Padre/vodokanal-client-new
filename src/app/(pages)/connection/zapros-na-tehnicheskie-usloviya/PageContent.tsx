f9d32453fb316e2e1a23f9efa7bc5b440c09aed3
Сергей Субботин <zarodiny@yandex.ru>
2025-08-11 10:52:33 +1000

    feat: добавляет табы

---
+++ src/app/(pages)/connection/zapros-na-tehnicheskie-usloviya/PageContent.tsx
@@ -0,0 +1,94 @@
+'use client'
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { useState } from 'react';
+
+import { StatementForm } from "@/app/components";
+
+import styles from "../style.module.scss";
+
+import type { Variants } from "framer-motion";
+
+type TabIndex = 0 | 1 | 2;
+
+const PageContent = () => {
+    const [activeTab, setActiveTab] = useState<TabIndex>(0);
+
+    const openTab = (e: React.MouseEvent<HTMLLIElement>) => {
+        const index = e.currentTarget.getAttribute('data-index');
+        if (index !== null) {
+            setActiveTab(parseInt(index) as TabIndex);
+        }
+    };
+
+    const variants: Variants = {
+        visible: {
+            opacity: 1,
+            height: "auto",
+            visibility: 'visible',
+            transition: {
+                when: "beforeChildren",
+                staggerChildren: 0.1,
+            },
+        },
+        hidden: {
+            opacity: 0,
+            height: 0,
+            visibility: 'hidden',
+        },
+    };
+
+    return (
+        <>
+            <ul className={styles.list}>
+                <li className={`${styles.item} ${activeTab === 0 ? styles.item_active : ""}`} tabIndex={0} data-index="0" onClick={openTab}>
+                    <span>Для физических лиц и ИЖС</span>
+                    <svg width="15" height="12" viewBox="0 0 15 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M13.8025 6.98638C14.0954 6.69349 14.0954 6.21862 13.8025 5.92572L9.02958 1.15275C8.73668 0.85986 8.26181 0.85986 7.96892 1.15275C7.67602 1.44565 7.67602 1.92052 7.96892 2.21341L12.2116 6.45605L7.96892 10.6987C7.67602 10.9916 7.67602 11.4665 7.96892 11.7594C8.26181 12.0522 8.73668 12.0522 9.02958 11.7594L13.8025 6.98638ZM0.345703 6.45605V7.20605H13.2722V6.45605V5.70605H0.345703V6.45605Z" />
+                    </svg>
+                </li>
+                <li className={`${styles.item} ${activeTab === 1 ? styles.item_active : ""}`} tabIndex={0} data-index="1" onClick={openTab}>
+                    <span>Для юридических лиц</span>
+                    <svg width="15" height="12" viewBox="0 0 15 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M13.8025 6.98638C14.0954 6.69349 14.0954 6.21862 13.8025 5.92572L9.02958 1.15275C8.73668 0.85986 8.26181 0.85986 7.96892 1.15275C7.67602 1.44565 7.67602 1.92052 7.96892 2.21341L12.2116 6.45605L7.96892 10.6987C7.67602 10.9916 7.67602 11.4665 7.96892 11.7594C8.26181 12.0522 8.73668 12.0522 9.02958 11.7594L13.8025 6.98638ZM0.345703 6.45605V7.20605H13.2722V6.45605V5.70605H0.345703V6.45605Z" />
+                    </svg>
+                </li>
+                <li className={`${styles.item} ${activeTab === 2 ? styles.item_active : ""}`} tabIndex={0} data-index="2" onClick={openTab}>
+                    <span>Для органов власти
+                        и самоуправления</span>
+                    <svg width="15" height="12" viewBox="0 0 15 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M13.8025 6.98638C14.0954 6.69349 14.0954 6.21862 13.8025 5.92572L9.02958 1.15275C8.73668 0.85986 8.26181 0.85986 7.96892 1.15275C7.67602 1.44565 7.67602 1.92052 7.96892 2.21341L12.2116 6.45605L7.96892 10.6987C7.67602 10.9916 7.67602 11.4665 7.96892 11.7594C8.26181 12.0522 8.73668 12.0522 9.02958 11.7594L13.8025 6.98638ZM0.345703 6.45605V7.20605H13.2722V6.45605V5.70605H0.345703V6.45605Z" />
+                    </svg></li>
+            </ul>
+
+            <motion.div
+                layout
+                variants={variants}
+                initial="hidden"
+                animate={activeTab === 0 ? "visible" : "hidden"}
+            >
+                <StatementForm />
+            </motion.div>
+            <motion.div
+                layout
+                variants={variants}
+                initial="hidden"
+                animate={activeTab === 1 ? "visible" : "hidden"}
+            >
+                111111111111
+            </motion.div>
+            <motion.div
+                layout
+                variants={variants}
+                initial="hidden"
+                animate={activeTab === 2 ? "visible" : "hidden"}
+            >
+                22222222222222222222
+            </motion.div>
+        </>
+
+    )
+
+}
+
+export default PageContent
