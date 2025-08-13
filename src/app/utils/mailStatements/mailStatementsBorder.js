53e0c3635a5eb7de805b03e1c6cd9986a29824a2
kkirrin <khlebnikov-2016@mail.ru>
2025-08-13 10:39:28 +1000

    refactor: add placeholders for new email functions in statement route and update label in StatementFormBorder component

---
+++ src/app/utils/mailStatements/mailStatementsBorder.js
@@ -0,0 +1,55 @@
+import nodemailer from 'nodemailer';
+
+const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: true, // true for port 465, false for other ports
+    auth: {
+        user: process.env.SMTP_USERNAME,
+        pass: process.env.SMTP_PASSWORD,
+    },
+});
+
+async function sendEmailBorder(body, files = []) {
+    try {
+        files.forEach((file, index) => {
+            console.log(`File ${index}:`, {
+                name: file.name,
+                type: file.type,
+                bufferSize: file.buffer ? file.buffer.length : 'undefined'
+            });
+        });
+
+        // Подготавливаем attachments для файлов
+        const attachments = files.map(file => ({
+            filename: file.name,
+            content: file.buffer,
+            contentType: file.type
+        }));
+
+        const info = await transporter.sendMail({
+            from: process.env.SMTP_FROM,
+            // to: `${process.env.SMTP_FROM}, Dubrovinaaa@yandex.ru`,
+            to: `${process.env.SMTP_FROM}, zarodiny@yandex.ru`,
+            subject: "Подключение к сетям",
+            text: `Имя: ${body.client_name}\nТелефон: ${body.client_phone}`,
+            html: `
+            <b>Имя:</b> ${body.client_name}<br>
+            <b>Номер телефона:</b> ${body.client_phone}<br>
+            <b>ФИО:</b> ${body.full_name}<br>
+            <b>Прошу выдать технические условия на проектирование узла учета потребляемой объекта:</b> ${body.address}<br><br>
+            <b>Подключение Объекта расположенного по адресу:</b> ${body.object_address}<br><br>
+            <b>Планируемая величина максимальной необходимой мощности (нагрузки) составляет для потребления холодной:</b> ${body.power}<br><br>
+            `,
+            attachments: attachments
+        });
+
+        // console.log("Message sent: %s", info.messageId);
+        return info;
+    } catch (error) {
+        console.error("Error sending email:", error);
+        throw error; // Перебрасываем ошибку, чтобы её можно было поймать в API роуте
+    }
+}
+
+export default sendEmailBorder;
