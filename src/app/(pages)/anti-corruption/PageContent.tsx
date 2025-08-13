7f48bd0781e10a011c9e0c4e8f948b2e2e30a4da
Сергей Субботин <zarodiny@yandex.ru>
2025-08-13 13:45:30 +1000

    feat: добавляет анимацию

--- src/app/(pages)/anti-corruption/PageContent.tsx
+++ src/app/(pages)/anti-corruption/PageContent.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { useEffect, useState, useRef } from 'react';
 
 import DocumentComponent from "@/app/components/DocumentComponent/DocumentComponent";
 
@@ -6,6 +7,34 @@ import styles from "./style.module.scss";
 import { PageContentProps } from "./types";
 
 export default function PageContent({ pageData }: PageContentProps) {
+    const [isVisible, setIsVisible] = useState(false);
+    const sectionRef = useRef<HTMLDivElement>(null);
+
+    const animateClassName = 'fade-up';
+
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                if (entry.isIntersecting && !isVisible) {
+                    setIsVisible(true);
+                    sectionRef.current?.classList.add('active');
+
+                    const animateItem = sectionRef.current?.querySelector('.animate');
+                    if (animateItem) {
+                        animateItem.classList.add('active');
+                    }
+             }
+            },
+            { threshold: 0.2 }
+        );
+
+        if (sectionRef.current) {
+            observer.observe(sectionRef.current);
+        }
+        return () => observer.disconnect();
+    }, [isVisible]);
+
     return (
         <>
             <section className={styles.section_item}>
@@ -38,8 +67,8 @@ export default function PageContent({ pageData }: PageContentProps) {
                 </ul>
             </section>
 
-            <section className={styles.section_item}>
-                <h2>{pageData?.title_3}</h2>
+            <section className={styles.section_item} ref={sectionRef}>
+                <h2 className='animate fade-up'>{pageData?.title_3}</h2>
                 <ul className={styles.documents_list}>
                     {pageData?.documents_3 && pageData?.documents_3.length > 0 &&
                         pageData.documents_3.map((document) => (
